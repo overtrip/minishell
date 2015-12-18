3f968c1b854ed83790805874e1215ae9e5c273dd
Jeremy ALONSO <jealonso@e3r11p10.42.fr>
2015-12-18 18:06:13 +0100

    env for fork update now

--- srcs/ft_fork.c
+++ srcs/ft_fork.c
@@ -6,25 +6,52 @@
 /*   By: jealonso <jealonso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/14 17:40:13 by jealonso          #+#    #+#             */
-/*   Updated: 2015/12/18 16:05:06 by jealonso         ###   ########.fr       */
+/*   Updated: 2015/12/18 18:01:49 by jealonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sh_1.h"
 
-static int	ft_fork(char *path, char **cmd)
+static char	**ft_convert_to_tab(t_list *local_env)
+{
+	int		i;
+	char	**tab;
+	char	**begin_tab;
+	t_list	*begin;
+
+	i = 0;
+	tab = NULL;
+	begin = local_env;
+	while (local_env && ++i)
+		local_env = local_env->next;
+	tab = (char **)malloc(sizeof(char *) * ++i);
+	begin_tab = tab;
+	while (begin)
+	{
+		*tab = ft_strdup(begin->data);
+		begin = begin->next;
+		++tab;
+	}
+	*tab = NULL;
+	return (begin_tab);
+}
+
+static int	ft_fork(char *path, char **cmd, t_list **local_env)
 {
 	pid_t	father;
+	char	**env;
 
+	env = ft_convert_to_tab(*local_env);
 	father = fork();
 	if (father > 0)
 	{
 		wait(NULL);
 		return (0);
 	}
-	if (father == 0)
+	else if (father == 0)
 	{
-		execve(path, cmd, NULL);
+		execve(path, cmd, env);
+		ft_free_tab(env);
 		exit(EXIT_SUCCESS);
 	}
 	return (-1);
@@ -44,12 +71,12 @@ int			ft_find(char *cmd, t_list **local_env)
 		return (-1);
 	tab = ft_strsplit(path, ':');
 	if (access(tmp[0], X_OK) != -1)
-		ft_fork(tmp[0], tmp);
+		ft_fork(tmp[0], tmp, local_env);
 	while (tab && *tab)
 	{
 		save = ft_strjoin(*tab, ft_strjoin("/", *tmp));
 		if (access(save, F_OK) != -1)
-			ft_fork(save, tmp);
+			ft_fork(save, tmp, local_env);
 		++tab;
 	}
 	free(save);
